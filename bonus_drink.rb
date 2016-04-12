8243d3854379e399c685c3227053fb3be6fc1903
etmnk <antilogic@outlook.com>
2016-04-12 21:21:03 +0900

    add code

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,46 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    trade_drink = TradeDrink.new(amount)
+    trade_drink.show
+  end
+end
+
+class TradeDrink
+  TRADABLE_BOTTLE = 3
+
+  def initialize(amount)
+    raise StandardError unless amount.is_a?(Integer)
+    raise StandardError if amount < 0
+    @drinkable_bottle = amount
+    @empty_bottle = 0
+    @drinked = 0
+    drink
+    solve
+  end
+
+  def show
+  	drink
+  	@drinked
+  end
+
+private
+
+  def solve
+    while @empty_bottle >= TRADABLE_BOTTLE
+      trade
+      drink
+    end
+  end
+
+  def trade
+    tradable = @empty_bottle / TRADABLE_BOTTLE
+    @drinkable_bottle += tradable
+    @empty_bottle -= tradable * TRADABLE_BOTTLE
+  end
+
+  def drink
+    @empty_bottle += @drinkable_bottle
+    @drinked += @drinkable_bottle
+    @drinkable_bottle = 0
   end
 end
